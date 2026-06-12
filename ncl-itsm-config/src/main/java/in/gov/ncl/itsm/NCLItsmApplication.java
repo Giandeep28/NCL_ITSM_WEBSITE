916e08e4c7fed2e359f25e0239dd32b48fbf238b
Giandeep Singh <giandeep.sgrl@gmail.com>
2026-06-12 12:42:06 +0530

    Initial commit

---
+++ ncl-itsm-config/src/main/java/in/gov/ncl/itsm/NCLItsmApplication.java
@@ -0,0 +1,11 @@
+package in.gov.ncl.itsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NCLItsmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NCLItsmApplication.class, args);
+    }
+}
